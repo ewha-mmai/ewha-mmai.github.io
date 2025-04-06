8c72eb16edabe45e597b6530bdbfa3d00d1918bf
ewha-mil <ewha.milab@gmail.com>
2025-04-06 16:15:00 +0000

    Deploying to gh-pages from @ ewha-mil/ewha-mil.github.io@d4c0ccd515682b68e31fc3670f8b1de4d13654fe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "$^\star$ equal contribution, $^\dagger$ corresponding author(s)",
+          description: "$^\star$ equal contribution",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
